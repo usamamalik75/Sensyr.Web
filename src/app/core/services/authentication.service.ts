a42441950d13a418292e81d3c3f29478c8335e15
Jahanzaib <jahanzaib.ahmed@xisys.co.uk>
2020-08-19 00:24:32 +0500

    scrollfix

--- src/app/core/services/authentication.service.ts
+++ src/app/core/services/authentication.service.ts
@@ -6,6 +6,6 @@ import { Injectable } from '@angular/core';
 export class AuthenticationService {
   token: string;
   constructor() {
-    this.token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IlMuQVJTSEFENjIzQEdNQUlMLkNPTSIsInVuaXF1ZV9uYW1lIjoiUy5BUlNIQUQ2MjNAR01BSUwuQ09NIiwiVXNlcklkIjoiMWYxMmM4MzUtZjE0OS00YTA5LWFiZmItNDE0YzA3MjYyZTYwIiwiQnJhbmNoSWQiOiIxIiwicm9sZSI6IlVzZXIiLCJuYmYiOjE1OTc3NzQ1NTksImV4cCI6MTU5Nzc3ODE1OSwiaWF0IjoxNTk3Nzc0NTU5fQ.J1NtpFhknwgPQxmaYifmJhyU7mliGb--2tyzDqr7vbE';
+    this.token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IlMuQVJTSEFENjIzQEdNQUlMLkNPTSIsInVuaXF1ZV9uYW1lIjoiUy5BUlNIQUQ2MjNAR01BSUwuQ09NIiwiVXNlcklkIjoiMWYxMmM4MzUtZjE0OS00YTA5LWFiZmItNDE0YzA3MjYyZTYwIiwiQnJhbmNoSWQiOiIxIiwicm9sZSI6IlVzZXIiLCJuYmYiOjE1OTc3Nzg0MjYsImV4cCI6MTU5Nzc4MjAyNiwiaWF0IjoxNTk3Nzc4NDI2fQ.y-IYoirBEd482V-VxjFjSkFpZMh6cDfjRyxczd4VtbY';
    }
 }
