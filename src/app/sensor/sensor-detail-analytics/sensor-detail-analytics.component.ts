96e1f5a0211a7bb5b95e05f1e82fe3a7281a19a6
Jahanzaib <jahanzaib.ahmad.76@gmail.com>
2020-08-26 21:23:48 +0500

    linechart

--- src/app/sensor/sensor-detail-analytics/sensor-detail-analytics.component.ts
+++ src/app/sensor/sensor-detail-analytics/sensor-detail-analytics.component.ts
@@ -40,14 +40,15 @@ export class SensorDetailAnalyticsComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.sensorId = +this.activatedRoute.snapshot.params.sensorId;
     this.getSensorDetailAnalyticsPerformance();
-    // this.getSensorDetailAnalyticsStatus();
-    this.getTestDetail();
+    this.getSensorDetailAnalyticsStatus();
+    // this.getTestDetail();
 
   }
 
   getSensorDetailAnalyticsPerformance() {
     this.sensorService.getSensorDetailAnalyticsPerformance(this.sensorId).subscribe(
       data => {
+        console.log(JSON.stringify(data.Data));
         this.lineGraph(data.Data);
       },
       error => {
@@ -57,7 +58,7 @@ export class SensorDetailAnalyticsComponent implements OnInit, OnDestroy {
   getSensorDetailAnalyticsStatus() {
     this.sensorService.getSensorDetailAnalyticsStatus(this.sensorId).subscribe(
       data => {
-        console.log(JSON.stringify(data.Data));
+        // console.log(JSON.stringify(data.Data));
         this.drawWithData(data);
       },
       error => {
@@ -67,7 +68,7 @@ export class SensorDetailAnalyticsComponent implements OnInit, OnDestroy {
   private getTestDetail() {
     this.sensorService.getTestDetail().subscribe(
       data => {
-        this.drawWithData(data);
+        this.lineGraph(data);
       });
   }
 
@@ -101,6 +102,66 @@ export class SensorDetailAnalyticsComponent implements OnInit, OnDestroy {
       const scrollbarX = new am4charts.XYChartScrollbar();
       scrollbarX.series.push(series);
       chart.scrollbarX = scrollbarX;
+
+      const bullet = series.bullets.push(new am4charts.CircleBullet());
+      bullet.circle.strokeWidth = 2;
+      bullet.circle.radius = 3;
+      bullet.circle.strokeOpacity = 0;
+      bullet.tooltipText = '{valueY}';
+      const bullethover = bullet.states.create('hover');
+      bullethover.properties.scale = 1.5;
+
+      bullet.adapter.add('fill', function (fill, target) {
+        if (target.dataItem['valueY'] <= 20) {
+          return am4core.color('#E87A7A');
+        }
+        else if (target.dataItem['valueY'] >= 21 && target.dataItem['valueY'] <= 40) {
+          return am4core.color('#FFBD2F');
+        }
+        else if (target.dataItem['valueY'] >= 41 && target.dataItem['valueY'] <= 60) {
+
+          return am4core.color('#5CB592');
+        }
+
+        else if (target.dataItem['valueY'] >= 61 && target.dataItem['valueY'] <= 80) {
+
+          return am4core.color('#FFBD2F');
+        }
+        else {
+          return am4core.color('#E87A7A');
+        }
+        return fill;
+      })
+
+      const range = valueAxis.createSeriesRange(series);
+      range.value = 0;
+      range.endValue = 20;
+      range.contents.stroke = am4core.color('#E87A7A');
+      range.contents.fill = range.contents.stroke;
+
+      const range1 = valueAxis.createSeriesRange(series);
+      range1.value = 21;
+      range1.endValue = 40;
+      range1.contents.stroke = am4core.color('#FFBD2F');
+      range1.contents.fill = range1.contents.stroke;
+
+      const range2 = valueAxis.createSeriesRange(series);
+      range2.value = 41;
+      range2.endValue = 60;
+      range2.contents.stroke = am4core.color('#5CB592');
+      range2.contents.fill = range2.contents.stroke;
+
+      const range3 = valueAxis.createSeriesRange(series);
+      range3.value = 61;
+      range3.endValue = 80;
+      range3.contents.stroke = am4core.color('#FFBD2F');
+      range3.contents.fill = range3.contents.stroke;
+
+      const range4 = valueAxis.createSeriesRange(series);
+      range4.value = 81;
+      range4.endValue = 1000;
+      range4.contents.stroke = am4core.color('#E87A7A');
+      range4.contents.fill = range4.contents.stroke;
 
       this.chart = chart;
     });
@@ -139,8 +200,8 @@ export class SensorDetailAnalyticsComponent implements OnInit, OnDestroy {
       this.valueAxis.renderer.labels.template.disabled = true;
       this.valueAxis.min = 0;
 
-      this.createSeries("WarningValue", "Warning");
-      this.createSeries("CriticalValue", "Critical");
+      this.createSeries('WarningValue', 'Warning');
+      this.createSeries('CriticalValue', 'Critical');
     });
   }
 
